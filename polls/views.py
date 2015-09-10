da9c28c85ae0b704883349716a471467aa2e9be6
Gary <kingofrabbit@sina.com>
2015-09-10 12:17:11 +0800

    20150920

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from polls.models import Poll
+from django.template import Context,loader
 
 def index(request):
-	return HttpResponse("Hello,world You`re at the poll index.")
+    #return HttpResponse("Hello,world You`re at the poll index.")
+    latest_poll_list=Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = Context({
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+    #output=','.join([v.question for v  in latest_poll_list])
+    #return HttpResponse(output)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
